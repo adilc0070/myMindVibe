b6d3a0f73ba07106a386e3ad2efd50bcfad7de3b
Adil C <adilc0070@gmail.com>
2025-01-12 23:11:26 +0530

    test

--- middlewares/mongoDb.js
+++ middlewares/mongoDb.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectToDatabase = async (dbName) => {
     try {
-        const uri = `mongodb://localhost:27017/${dbName}`;
+        const uri = `mongodb://localhost:27017/${dbName}`;    
         await mongoose.connect(uri);
         console.log('Connected to MongoDB successfully');
     } catch (error) {
